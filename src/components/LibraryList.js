646be628c966967246367be02cf2231be56b1e7a
deOliveiraDiego <Diego.oliveira.silva.07@gmail.com>
2018-03-16 18:50:30 -0300

    Começar a funcionalizar a opção de hidden/show text

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,16 +4,18 @@ import { connect } from 'react-redux';
 import ListItem from './ListItem'
 
 class LibraryList extends Component {
-  componentDidMount() {
+
+  componentWillMount() {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
 
-    this.DataSource = ds.cloneWithRows(this.props.libraries);
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
 
   renderRow(library) {
     return <ListItem library={library} />
+    console.log("Test")
   }
 
   render() {
@@ -22,13 +24,12 @@ class LibraryList extends Component {
         dataSource={this.dataSource}
         renderRow={this.renderRow}
       />
-
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { libraries: state.libraries }
+  return { libraries: state.libraries };
 }
 
 export default connect(mapStateToProps)(LibraryList);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native'
-import { CardSection } from './common'
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
 
 class ListItem extends Component {
   render() {
-    <CardSection>
-      <Text>{this.props.library.title} </Text>
-    </CardSection>
+
+    const { titleStyle } = styles;
+    console.log(this.props);
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    )
   }
 }
 
-export default ListItem;
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+export default connect(null, actions)(ListItem);
